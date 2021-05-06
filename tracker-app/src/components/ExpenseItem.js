34eaddf8d27cabd0f9a9778dcd9e4077e5117452
Andy <andyvelazquez13@yahoo.com>
2021-05-06 08:07:32 -0700

    add first component

---
+++ tracker-app/src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  const expenseDate = new Date(2021, 3, 28);
+  const expenseTitle = 'car Insurance';
+  const expenseAmount = '294.65';
+  return (
+
+    <h2> Expense item </h2>
+  )
+}
+
+export default ExpenseItem;
