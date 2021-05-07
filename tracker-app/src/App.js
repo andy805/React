03e323f6b4a8dabc8cea1c9d64c93d91dcf36906
Andy <andyvelazquez13@yahoo.com>
2021-05-06 21:21:54 -0700

    add prop, add object handling from prop, prop is title, amount, date

--- tracker-app/src/App.js
+++ tracker-app/src/App.js
@@ -1,13 +1,37 @@
-import ExpenseItem from './components/ExpenseItem'
+import ExpenseItem from "./components/ExpenseItem.js";
+
+const expensesArray = [
+  {
+    title: "fireball handle",
+    amount: "24.99",
+    date: new Date()
+  },
+  {
+    title: "round table pizza",
+    amount: "21.99",
+    date: new Date()
+  },
+  {
+    title: "gas",
+    amount: "44.99",
+    date: new Date()
+  },
+  {
+    title: "flowers",
+    amount: "84.99",
+    date: new Date()
+  },
+];
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p> This is also visible </p>
-      <ExpenseItem></ExpenseItem>
+      <h2> Let 's get started!</h2> <p> This is also visible </p>
+      <ExpenseItem arrayObj={expensesArray[0]}> </ExpenseItem>
+      <ExpenseItem arrayObj={expensesArray[1]}> </ExpenseItem>
+      <ExpenseItem arrayObj={expensesArray[2]}> </ExpenseItem>
+      <ExpenseItem arrayObj={expensesArray[3]}> </ExpenseItem>
     </div>
   );
 }
-
 export default App;
