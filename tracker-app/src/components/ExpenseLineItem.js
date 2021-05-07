7a1acc0fde287161cde65c52447a67cbeac1abfe
Andy <andyvelazquez13@yahoo.com>
2021-05-07 08:12:08 -0700

    rename expenseItem to expenseLineItem, add component expenseDateItem

---
+++ tracker-app/src/components/ExpenseLineItem.js
@@ -0,0 +1,14 @@
+import './ExpenseLineItem.css';
+import ExpenseDateItem from './ExpenseDateItem';
+
+function ExpenseLineItem(prop) {
+  return (
+    <div className="expenseLineItem">
+      <ExpenseDateItem date={prop.arrayObj.date} />
+      <h2> {prop.arrayObj.title} </h2>
+      <p> ${prop.arrayObj.amount} </p>
+    </div>
+  );
+}
+
+export default ExpenseLineItem;
