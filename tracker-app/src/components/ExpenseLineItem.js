211df32dd9b7ab40bbdabdb51d7bb2532ca4694e
Andy <andyvelazquez13@yahoo.com>
2021-05-08 20:28:10 -0700

    add styling to header

--- tracker-app/src/components/ExpenseLineItem.js
+++ tracker-app/src/components/ExpenseLineItem.js
@@ -5,7 +5,7 @@ function ExpenseLineItem(prop) {
   return (
     <div className="expenseLineItem">
       <ExpenseDateItem date={prop.arrayObj.date} />
-      <h2> {prop.arrayObj.title} </h2>
+      <h2 className="expenseLineItemDescription"> {prop.arrayObj.title} </h2>
       <p> ${prop.arrayObj.amount} </p>
     </div>
   );
