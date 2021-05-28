8cea86f7521966ed39b3daf4cbee89af0eb00579
Andy <andyvelazquez13@yahoo.com>
2021-05-27 19:39:35 -0700

    add state hook

--- tracker-app/src/components/Expenses/ExpenseLineItem.js
+++ tracker-app/src/components/Expenses/ExpenseLineItem.js
@@ -1,13 +1,24 @@
+
+import React, {useState} from 'react';
 import './ExpenseLineItem.css';
 import ExpenseDateItem from './ExpenseDateItem';
 import Card from '../UI/Card'
 
 function ExpenseLineItem(prop) {
+
+  const [title, setTitle] = useState(prop.arrayObj.title);
+
+  function clickHandler() {
+    setTitle("updated");
+    console.log(title);
+  }
+
   return (
     <Card className="expenseLineItem">
       <ExpenseDateItem date={prop.arrayObj.date} />
-      <h2 className="expenseLineItemDescription"> {prop.arrayObj.title} </h2>
+      <h2 className="expenseLineItemDescription"> {title} </h2>
       <p> ${prop.arrayObj.amount} </p>
+      <button onClick={clickHandler}> click me </button>
     </Card>
   );
 }
