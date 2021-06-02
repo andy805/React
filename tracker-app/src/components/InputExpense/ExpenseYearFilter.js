d0edf1e6c44d70d5f4cdbdb3ccb24cdee2434073
Andy <andyvelazquez13@yahoo.com>
2021-06-02 07:24:14 -0700

    remove state from component. Is not needed

--- tracker-app/src/components/InputExpense/ExpenseYearFilter.js
+++ tracker-app/src/components/InputExpense/ExpenseYearFilter.js
@@ -1,11 +1,11 @@
-import React,{useState} from 'react';
+// import React,{useState} from 'react';
 
 const ExpenseYearFilter = (props) => {
 
-  const [year, setYear] = useState("");
+  // const [year, setYear] = useState("");
   const filterYearHandler = (event) => {
     console.log(event.target.value);
-    setYear(event.target.value);
+    // setYear(event.target.value);
     // call parent function
     props.filter(event.target.value);
   };
@@ -15,7 +15,7 @@ const ExpenseYearFilter = (props) => {
     <label>
       filter by year
     </label>
-    <select onChange={filterYearHandler}>
+    <select value={props.selectedYear} onChange={filterYearHandler} >
       <option value="2022">2022</option>
       <option value="2021">2021</option>
       <option value="2020">2020</option>
