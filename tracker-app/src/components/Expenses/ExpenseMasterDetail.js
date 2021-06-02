e12943d3e48e98fcdbf8760f479a34f74e0ca8d6
Andy <andyvelazquez13@yahoo.com>
2021-06-02 07:25:04 -0700

    add filtering by year to component

--- tracker-app/src/components/Expenses/ExpenseMasterDetail.js
+++ tracker-app/src/components/Expenses/ExpenseMasterDetail.js
@@ -4,17 +4,29 @@ import ExpenseYearFilter from "../InputExpense/ExpenseYearFilter.js";
 import Card from "../UI/Card.js";
 import "./ExpenseMasterDetail.css";
 
+
 function ExpenseMasterDetail(prop) {
-  const [filterYear, setFilterYear] = useState("");
+  const [filterYear, setFilterYear] = useState("2021");
   const filterYearHandler = (year) => {
     console.log(year);
     setFilterYear(year);
   };
+  function filterByYear(expense) {
+    let year = expense.date.getFullYear().toString();
+    console.log(year);
+    if(year === filterYear){
+      console.log("true");
+      return true;
+      // return ( (<ExpenseLineItem key={expense.id} arrayObj={expense}/>));
+    }
+    return false;
+  }
   return (
     <div>
-      <ExpenseYearFilter filter={filterYearHandler} />
+      <ExpenseYearFilter selectedYear={filterYear} filter={filterYearHandler} />
       <Card className="ExpenseMasterDetail">
-        {prop.arrObj.map((expense) => (<ExpenseLineItem key={expense.id} arrayObj={expense}/>))}
+        {prop.arrObj.filter(filterByYear).map(expense => {return(<ExpenseLineItem key={expense.id} arrayObj={expense} />)})}
+        {/* {prop.arrObj.map(expense => {return (<ExpenseLineItem key={expense.id} arrayObj={expense} />)})} */}
         {/* <ExpenseLineItem arrayObj={prop.arrObj[0]} />
         <ExpenseLineItem arrayObj={prop.arrObj[1]} />
         <ExpenseLineItem arrayObj={prop.arrObj[2]} />
